cfacd21da6eac2881ca36c53227ed7540418e8fe
rakib-cse-sust <rakib.cse.sust@gmail.com>
2016-05-24 00:52:54 +0600

    routing

--- Modules/Account/angular/Account.module.js
+++ Modules/Account/angular/Account.module.js
@@ -1,15 +1,16 @@
 ﻿var accountApp = angular.module('accountApp', ['ui.router'])
-.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
+    $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise('/signin');
 
     $stateProvider
-    .state('signin', {
-        url: '/signin',
-        templateUrl: '/Modules/Account/angular/partials/partial-signin.html'
+    .state('signin', {  
+        url: '/', //url: '/signin',
+        templateUrl: 'Modules/Account/angular/partials/partial-signin.html'
     })
     .state('registration', {
         url: "/registration",
-        templateUrl: '/Modules/Account/angular/partials/partial-registration.html'
+        templateUrl: 'Modules/Account/angular/partials/partial-registration.html'
     });
 }]);
